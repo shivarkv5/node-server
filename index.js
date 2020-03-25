c65aeb3d529f97813df25ff85d8fbbf05eec1878
srkvodnala <srkrao547@gmail.com>
2020-03-24 22:07:31 -0500

    host & port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 80;
+const hostname = process.env.hostname || '0.0.0.0';
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
